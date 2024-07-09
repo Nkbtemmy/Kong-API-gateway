acd1fbff1c7573cf2e284b9450c81372c7ac6cec
nkbtemmy <emmanuel.nkubito@amalitech.com>
2024-07-09 18:55:01 +0200

    initial setup

---
+++ Makefile
@@ -0,0 +1,5 @@
+start:
+	docker compose up --build
+dev:
+	docker compose -f docker-compose.no.yaml up --build
+	
---
+++ init_kong.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Wait for Kong to be ready
+until $(curl --output /dev/null --silent --head --fail http://kong:8001); do
+  printf '.'
+  sleep 5
+done
+
+# Add a service
+curl -i -X POST --url http://kong:8001/services/ --data 'name=web-service' --data 'url=http://web:5000'
+
+# Add a route for the service
+curl -i -X POST --url http://kong:8001/services/web-service/routes --data 'hosts[]=localhost'
